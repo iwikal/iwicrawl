2d6b18b10bb2d2c40f655202fda1988ada6cf69c
iwikal <e.joel.nordstrom@gmail.com>
2019-03-04 01:29:23 +0100

    Logging

--- src/fetch.rs
+++ src/fetch.rs
@@ -4,72 +4,74 @@ use futures::future::*;
 use url::Url;
 use hyper::{Client, rt::Stream};
 
-fn extract_subdirs(body: hyper::Chunk, url: Url) -> SubdirTok {
+fn extract_subdirs(body: hyper::Chunk, url: Url) -> Result<SubdirTok, CliError> {
     let s = std::str::from_utf8(&body)
-        .unwrap();
-    SubdirTok::from_body(url, s)
+        .map_err(|e| CliError(format!("error parsing body: {}", e)))?;
+    Ok(SubdirTok::from_body(url, s))
 }
 
 pub fn traverse(client: &'static Client<hyper::client::HttpConnector>, url: Url)
-    -> Box<dyn Future<Item=u32, Error=CliError> + Send> {
+    -> Box<dyn Future<Item=u32, Error=CliError> + Send>
+{
     let f = loop_fn(url, move |url: Url| {
         use hyper::Uri;
         let uri: Uri = url.to_string().parse().unwrap();
-        client
-            .get(uri)
-            .map_err(|e| {
-                CliError(format!("{}", e))
-            })
+        client.get(uri)
+            .map_err(|e| CliError(format!("{}", e)))
             .and_then(move |res| {
-                println!("got     {}:\n{:#?}", url, res);
+                debug!("{}:\n{:#?}", url, res);
                 let status = res.status();
                 let headers = res.headers();
 
+                use hyper::header::*;
                 if status.is_success() {
-                    let f = res.into_body()
-                        .concat2()
-                        .map_err(|e| {
-                            CliError(format!("{}", e))
-                        })
-                    .map(move |body| (body, url));
-                    Ok(Loop::Break(f))
+                    let content_type = headers
+                        .get(CONTENT_TYPE)
+                        .ok_or_else(|| CliError(format!("Content type missing")))?
+                        .to_str()
+                        .map_err(|e| CliError(format!("{}", e)))?;
+                    if content_type.starts_with("text/html") {
+                        let f = res.into_body()
+                            .concat2()
+                            .map_err(|e| CliError(format!("{}", e)))
+                            .map(move |body| {
+                                let subdirs = extract_subdirs(body, url).unwrap();
+                                let SubdirTok {
+                                    paths,
+                                    current_url,
+                                } = subdirs;
+                                let subfutures = paths
+                                    .into_iter()
+                                    .map(move |path| {
+                                        let path_str = &path.to_string();
+                                        let url = current_url.join(path_str).unwrap();
+                                        traverse(client, url)
+                                    });
+                                join_all(subfutures)
+                                    .map(|subdirs| {
+                                        subdirs.iter()
+                                            .fold(0, |acc, cur| acc + cur)
+                                    })
+                            });
+                        Ok(Loop::Break(Either::A(f.flatten())))
+                    } else {
+                        println!("{}", url);
+                        Ok(Loop::Break(Either::B(futures::future::ok(1))))
+                    }
                 } else if status.is_redirection() {
-                    use hyper::header::*;
                     let s = headers[LOCATION]
                         .to_str()
-                        .map_err(|e| {
-                            CliError(format!("{}", e))
-                        })?;
-                    let url = Url::parse(s)
-                        .map_err(|e| {
-                            CliError(format!("{}", e))
-                        })?;
-                    Ok(Loop::Continue(url))
+                        .map_err(|e| CliError(format!("{}", e)))?;
+                    let new_url = Url::parse(s)
+                        .map_err(|e| CliError(format!("{}", e)))?;
+                    info!("{} redirected to {}: {}", url, new_url, status);
+                    Ok(Loop::Continue(new_url))
                 } else {
-                    Err(CliError(format!("Error: status {}", status)))
+                    Err(CliError(format!("{} {}", status, url)))
                 }
-
             })
-        })
-        .flatten()
-        .and_then(move |(body, url)| {
-            let SubdirTok {
-                paths,
-                current_url,
-            } = extract_subdirs(body, url);
-            let subfutures = paths
-                .into_iter()
-                .map(move |path| {
-                    let path_str = &path.to_string();
-                    let url = current_url.join(path_str).unwrap();
-                    traverse(client, url)
-                });
-            join_all(subfutures)
-                .map(|subdirs| {
-                    subdirs.iter()
-                        .fold(0, |acc, cur| acc + cur)
-                })
-        });
+    })
+        .flatten();
 
     Box::new(f)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![warn(bare_trait_objects)]
 
 extern crate clap;
+#[macro_use] extern crate log;
+extern crate stderrlog;
 extern crate hyper;
 extern crate robots_txt;
 extern crate exit;
@@ -24,6 +26,10 @@ fn main() -> Exit<CliError> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .author(env!("CARGO_PKG_AUTHORS"))
+        .arg(Arg::with_name("verbosity")
+             .short("v")
+             .multiple(true)
+             .help("increase message verbosity"))
         .arg(Arg::with_name("URI")
             .index(1)
             .help("the uri of the directory to crawl")
@@ -31,19 +37,27 @@ fn main() -> Exit<CliError> {
             .takes_value(true))
         .get_matches();
 
+    let verbose = matches.occurrences_of("verbosity") as usize;
+    stderrlog::new()
+        .module(module_path!())
+        // .quiet(quiet)
+        .verbosity(verbose)
+        // .timestamp(ts)
+        .init()
+        .unwrap();
+
     let uri = matches.value_of("URI").unwrap();
     let current_url = Url::parse(uri)
-        .map_err(|_| {
-            CliError(format!("Uri has no authority: '{}'", uri))
+        .map_err(|e| {
+            CliError(format!("'{}': {}", uri, e))
         })?;
     let client = Client::new();
     let client = Box::leak(Box::new(client));
     let fut = fetch::traverse(client, current_url)
-        .map_err(|e| eprintln!("{}", e))
+        .map_err(|e| error!("{}", e))
         .map(|n| {
             println!("Result: {}", n);
         });
     rt::run(fut);
-    println!("Done");
     Exit::Ok
 }
